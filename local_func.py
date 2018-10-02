0aa7dd6e9f513998f2a83b6a44d5e16a647f7938
chengbin <sbkim0407@gmail.com>
2018-10-02 20:45:35 +0900

    locals() func

---
+++ local_func.py
@@ -0,0 +1,21 @@
+# print(locals())
+
+
+# locals() inside function
+def fx1():
+    var = 1
+    global gl
+    gl = 'x'
+    print('\nlocals() value inside function\n', locals())
+    print('\nglobals() value inside function\n', globals())
+
+
+# fx1()
+
+
+# locals() inside class
+class Data:
+    x = 0
+    print('\nlocals() value inside class\n', locals())
+    print('\nglobals() value inside class\n', globals())
+
